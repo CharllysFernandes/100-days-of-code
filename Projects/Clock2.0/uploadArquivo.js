ed2f2f3483f89efe327848d75789eb435fa238ce
Charllys Fernandes <charllys.daiany@gmail.com>
2022-12-21 00:20:52 -0300

    Create uploadArquivo.js

---
+++ Projects/Clock2.0/uploadArquivo.js
@@ -0,0 +1,10 @@
+function fecharModal() {
+    document.getElementById("uploadModal").style.display = "hidden";
+}
+const uploadArquivo = document.getElementById("upload")
+
+
+uploadArquivo.addEventListener("click", function () {
+    fecharModal()
+   console.log("fasdfasd")
+})
