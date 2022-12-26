45caca19585112ba44e96fbc678fd3073ccc373f
Charllys Fernandes <charllys.daiany@gmail.com>
2022-12-26 00:03:31 -0300

    update clock

--- Projects/Clock2.0/script.js
+++ Projects/Clock2.0/script.js
@@ -8,16 +8,82 @@ function notShow(htmlId) {
 }
 
 function baixarArquivoLocal(name, dados) {
+  console.log(dados)
   let link = document.createElement('a');
-  link.href = 'data:application/octet-stream;charset=utf-8,' + JSON.stringify(dados);
+  link.href = 'data:application/octet-stream;charset=utf-8,' + JSON.stringify(dados, null, 2);
   link.download = name;
   link.click();
 }
 
+function createLog() {
+  sectionShowOff.innerHTML +=
+    `
+  <input class="fs-5" type="date" id="date">
+  <div id="dateErro" class="text-warning"></div>
+  <input class="fs-4" type="text" id="logMessge" maxlength="50" placeholder="create a log message">
+  <div id="msgErro" class="text-warning"></div>
+  <button class="btn-primary btn text-capitalize" id="createJsonFile">create JSON file</button>
+  `
+}
+
+function showButton(msgState, buttonId) {
+  sectionShowOff.innerHTML +=
+    `
+    <button type="button" class="btn btn-outline-primary" id="${buttonId}">${msgState}</button>
+    `
+}
+
+function showClock() {
+  setInterval(() => {
+    getHours()
+  }, 1000)
+
+  sectionShowOff.innerHTML =
+    `
+  <h1 class="fw-lighter text-white fs-4">100 Days of Code</h1>
+  <div class="my-2 clock fw-lighter border border-0 rounded-pill bg-info fw-bold text-center fs-1 p-2 w-50 text-white"
+  id="relogio"></div>
+  `
+}
+
+function showTableEmpty() {
+  sectionShowOff.innerHTML +=
+    `<div class="w-100 h-75 overflow-scroll">
+  <table class="table table-dark table-striped fw-lighter" id="tableList">
+  <thead>
+      <tr>
+          <th scope="col">Dia</th>
+          <th scope="col">Log</th>
+          <th scope="col">Data</th>
+          <th scope="col"></th>
+      </tr>
+  </thead>
+  <tbody id="content">
+  </tbody>
+  </table>
+  </div>
+  `
+}
+
+function showTableContent(intern) {
+  for (let i = 0; i < intern.length; i++) {
+    let content = document.getElementById("content");
+    content.innerHTML +=
+      `
+    <tr>
+    <th scope="row">${intern[i].id}</th>
+    <td>${intern[i].log.replaceAll("_", " ")}</td>
+    <td>${intern[i].msgDate}</td>
+    </tr>
+    `
+
+  }
+}
 
 function showTable(id, log, date) {
-  sectionShowOff.innerHTML += 
-  `
+  sectionShowOff.innerHTML +=
+    `
+    <div class="w-100 h-75 overflow-scroll">
   <table class="table table-dark table-striped fw-lighter" id="tableList">
   <thead>
       <tr>
@@ -30,7 +96,7 @@ function showTable(id, log, date) {
   <tbody>
     <tr>
       <th scope="row">${id}</th>
-      <td>${log}</td>
+      <td>${log.replaceAll("_", " ")}</td>
       <td>${date}</td>
    <td id="btnCompatilhar">
    <a href="#" target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
@@ -39,6 +105,8 @@ function showTable(id, log, date) {
    </td>
    </tr>
   </tbody>
+  </table>
+  </div>
   `
 }
 
@@ -59,79 +127,69 @@ buttonNotDB.addEventListener("click", function () {
   console.log("not database create one...")
   notShow("sectionOne");
 
-  sectionShowOff.innerHTML =
-    `
-    <input class="fs-5" type="date" id="date">
-    <div id="dateErro" class="text-warning"></div>
-    <input class="fs-4" type="text" id="logMessge" maxlength="50" placeholder="create a log message">
-    <div id="msgErro" class="text-warning"></div>
-    <button class="btn-primary btn text-capitalize" id="createJsonFile">create JSON file</button>
-    `
+  createLog();
+
   // create Json File
   document.getElementById("createJsonFile").addEventListener("click", function () {
     // create body of json
     let msgDate = document.getElementById("date").value;
-    let log = document.getElementById("logMessge").value;
-    const id = "1";
-
+    let log = document.getElementById("logMessge").value.replaceAll(" ", "_");
+    const id = 1;
     const firstDay = [{ id, log, msgDate }];
-    console.log(firstDay[0])
-
+    console.log(firstDay)
     baixarArquivoLocal('100DaysOfCode.json', firstDay)
 
     // Show Off Clock
-    sectionShowOff.innerHTML =
-      `
-    <div class="my-2 clock fw-lighter border border-0 rounded-pill bg-info fw-bold text-center fs-1 p-2 w-50 text-white"
-    id="relogio"></div>
-    `
+    // active clock
+    showClock();
+
 
     // Show button initial now
 
-    sectionShowOff.innerHTML += 
-    `
-    <button type="button" class="btn btn-outline-primary" id="initial">Começar agora</button>
-    `
-
+    showButton("Começar Agora", "initial");
 
     document.getElementById("initial").addEventListener("click", function () {
-    console.log("funcionou..")
-    showTable(id, log, msgDate);
+      console.log("funcionou..")
+      showTable(id, log, msgDate);
 
-    let minutos = 0;
-        let porcentagemBarra = 0;
-        cron = setInterval(() => { timer(); }, 60000); // para fazer em minutos
+      let minutos = 0;
+      let porcentagemBarra = 0;
+      cron = setInterval(() => { timer(); }, 60000); // para fazer em minutos
 
-        function timer() {
-            if (minutos <= 60) {
-                minutos++
-                porcentagemBarra += 100 / 60; // 100% / 60 minutos
-                console.log(porcentagemBarra)
-                document.getElementById("progress-bar").style.width = `${porcentagemBarra}%`
-            }
+      function timer() {
+        if (minutos <= 60) {
+          minutos++
+          porcentagemBarra += 100 / 60; // 100% / 60 minutos
+          console.log(porcentagemBarra)
+          document.getElementById("progress-bar").style.width = `${porcentagemBarra}%`
+
         }
-        sectionShowOff.innerHTML += `
-
-        <div class="progress">
-        <div id="progress-bar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: ${parseFloat(barraProgresso)}%"></div>
+      }
+      sectionShowOff.innerHTML +=
+        `
+        <div class="progress d-flex w-100">
+        <div id="progress-bar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:0%"></div>
         </div>
         `
 
     })
-
-
-    // console.log("Mostrar barra de progresso.");
   })
 })
 
 // YES DATABASE
 
 buttonYesDB.addEventListener("click", function () {
+  notShow("sectionOne");
+  // Show Off Clock
+  showClock();
+
   console.log("Database yes, upload then..");
-  console.log(presentJSON)
+  // console.log(presentJSON)
 
   // Read file .json
   document.getElementById("uploadJSON").addEventListener("click", function () {
+    showTableEmpty();
+
     console.log("click uploadJson");
     var jsonUploaded = document.getElementById('fileInput').files[0];
     usingJSON(jsonUploaded)
@@ -145,34 +203,56 @@ function usingJSON(databaseJSON) {
   jsonReader.onload = function (e) {
     var content = e.target.result;
     var intern = JSON.parse(content); // parse json 
-    console.log(intern[0].id); // You can index every object
 
     // TODO
     // PROCESS JSON INTO LIST
+    for (let i = 0; i < intern.length; i++) {
+      let content = document.getElementById("content");
+      content.innerHTML +=
+        `
+      <tr>
+      <th scope="row">${intern[i].id}</th>
+      <td>${intern[i].log.replaceAll("_", " ")}</td>
+      <td>${intern[i].msgDate}</td>
+      </tr>
+      `
+
+    }
+    showButton("Começar Agora", "increment");
+    increment.addEventListener('click', function () {
+      sectionShowOff.innerHTML = ``;
+      // show create log
+      console.log("show create log");
+      createLog();
+
+      document.getElementById("createJsonFile").addEventListener("click", function () {
+        console.log("incrementJsonFile");
+        let id = intern.length
+        id++ // add with day
+        let msgDate = document.getElementById("date").value;
+        // let log = toString(document.getElementById("logMessge").value);
+        let log = document.getElementById("logMessge").value.replaceAll(" ", "_");
+
+        console.log(id, msgDate, log);
+
+        intern.push({ id, log: `${log}`, msgDate })
+        baixarArquivoLocal('100DaysOfCode.json', intern)
+
+        console.log(intern)
+
+        showClock();
+        showButton("Start And Save JSON");
+        showTableEmpty();
+        showTableContent(intern);
+      })
+    })
+
 
   };
   jsonReader.readAsText(databaseJSON);
 
 }
 
-
-
-// comecarAgora.addEventListener("", function () {
-//     // const date = new Date();
-//     // let msgDate = `${date.getDate()}/${date.getUTCMonth() + 1}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}h`
-//     // let log = document.getElementById("floatingTextarea").value
-//     let id = 1;
-//     const firstDay = [{ id, msgDate, log, }]
-
-//     //criar o link de compartilhamento.
-//     const tweet = `https://twitter.com/intent/tweet?text=Day%20${id},%20100DaysOfCode%20Challenge%20&hashtags=100DaysOfCode`
-//     const list = document.getElementById("tableList");
-//     list.innerHTML += `
-//     
-//     `;
-
-
-// })
 
 //=======================================
 // Clock
@@ -190,9 +270,7 @@ const getHours = () => {
   relogio.innerHTML = `${hour}:${minute}:${second}`
 }
 
-setInterval(() => {
-  getHours()
-}, 1000)
+
 
 // End Clock
 //=========================================
